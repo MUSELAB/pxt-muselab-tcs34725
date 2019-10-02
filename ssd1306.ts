fc4976f929932dcf75de087564c2f6e42757989c
MUSELABA5 <ken.lai@muselab.cc>
2019-10-02 17:27:57 +0800

    test

--- ssd1306.ts
+++ ssd1306.ts
@@ -1,61 +1,78 @@
 //% icon="\uf26c"
 //% color="#F59E20" weight="99"
-namespace MuseOLED {
+namespace MuseColorRecognition {
 
     /**
-     * initialises the i2c OLED display
-     * @param height height (in pixels)
-     * @param width width (in pixels)
+     * initialises the i2c TCS34725 display
      */
-    //% blockId=oled_init_terminal
-    //% block="initialize OLED with height %height|width %width"
+    //% blockId=TCS34725_init_terminal
+    //% block="initialize TCS34725"
     //% icon="\uf1ec" 
-    //% shim=OLED::init_terminal
+    //% shim=pxt_tcs34725::TCS34725_Init
 	//% weight=90	
 	//% blockGap=8
-    export function init(height: number, width: number): void {
+    export function init(): void {
         return;
     }
 
-    /**
-     * clears the screen.
-     */
-    //% blockId=oled_clear_screen
-    //% block="clear OLED display"
-    //% icon="\uf1ec" 
-    //% shim=OLED::clearDisplay
-    export function clear(): void {
-        return;
-    }
 
     /**
-     * prints a string on the OLED display
-     * @param text text to display
+     * Get TCS34725 RGB data
      */
     //% weight=87
-    //% block="show OLED string %text" 
+    //% block="Get_RGB" 
     //% async
-    //% blockId=oled_print_string
+    //% blockId=Get_TCS34725_RGB_Data
     //% icon="\uf1ec"
-    //% shim=OLED::showString
+    //% shim=pxt_tcs34725::Get_Data
 	//% weight=85	
 	//% blockGap=8
-    export function showString(text: string): void {
-        console.log("display: " + text);
+    export function Get(): void {
         return;
     }
 
-    /**
-     * prints a number on the OLED display
-     * @param number number to display
+        /**
+     * prints a  TCS34725 red data
      */
-    //% weight=80
-    //% blockId=oled_print_number
-    //% block="show OLED number %number"
-    //% async 
-    //% shim=OLED::showNumber
-    export function showNumber(number: number): void {
-        console.log("display: " + number);
+    //% weight=87
+    //% block="Show_Red_Number" 
+    //% async
+    //% blockId=Show_Red_Number
+    //% icon="\uf1ec"
+    //% shim=pxt_tcs34725::Get_Red
+	//% weight=85	
+	//% blockGap=8
+    export function Show_Red_Number(): void {
+        return;
+    }
+
+        /**
+     * prints a TCS34725 green data
+     */
+    //% weight=87
+    //% block="Show_Green_Number" 
+    //% async
+    //% blockId=Show_Green_Number
+    //% icon="\uf1ec"
+    //% shim=pxt_tcs34725::Get_Green
+	//% weight=85	
+	//% blockGap=8
+    export function Show_Green_Number(): void {
+        return;
+    }
+
+        /**
+     * prints a TCS34725 Blue data
+     */
+    //% weight=87
+    //% block="Show_Blue_Number" 
+    //% async
+    //% blockId=Show_blue_Number
+    //% icon="\uf1ec"
+    //% shim=pxt_tcs34725::Get_Blue
+	//% weight=85	
+	//% blockGap=8
+    export function Show_Blue_Number(): void {
         return;
     }
 }
